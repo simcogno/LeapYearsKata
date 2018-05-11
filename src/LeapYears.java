0ecc3fa4b07d052446b958e792ed2dcf6feac40e
Simone <simcogno@gmail.com>
2018-05-11 11:55:47 +0200

    All tests passes

--- src/LeapYears.java
+++ src/LeapYears.java
@@ -2,7 +2,11 @@
 public class LeapYears {
 
 	public boolean isLeapYear(int year) {
-		if(year % 400 == 0)
+		
+		if(year < 1582)
+			return false;
+		
+		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 			return true;
 		else
 			return false;
--- src/LeapYearsTest.java
+++ src/LeapYearsTest.java
@@ -1,19 +1,56 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearsTest {
 
-	@Test
-	public void ifYearIsDivisibleBy400ThanIsLeapYear() {
-		LeapYears leapYears = new LeapYears();
-		assertTrue(leapYears.isLeapYear(400));
+	private LeapYears leapYears;
+	
+	@Before
+	public void setUp() throws Exception {
+		leapYears = new LeapYears();
 	}
 	
 	@Test
-	public void ifYearIsNotDivisibleBy400ThanIsNotLeapYear() {
-		LeapYears leapYears = new LeapYears();
-		assertFalse(leapYears.isLeapYear(1000));
+	public void aYearBeforTheStartOfGregorianCalendarCanNotBeLeap() {
+		assertFalse(leapYears.isLeapYear(800));
+		assertFalse(leapYears.isLeapYear(4));
+	}
+
+	@Test
+	public void ifYearIsDivisibleBy400ThanIsLeapYear() {
+		assertTrue(leapYears.isLeapYear(1600));
+		assertTrue(leapYears.isLeapYear(2000));
+		assertTrue(leapYears.isLeapYear(2400));
+	}
+	
+	@Test
+	public void ifDivisibleBy100ShouldAlsoBeDivisibleBy400ToBeLeapYear() {
+		assertFalse(leapYears.isLeapYear(1700));
+		assertFalse(leapYears.isLeapYear(1800));
+		assertFalse(leapYears.isLeapYear(1900));
+		assertFalse(leapYears.isLeapYear(2100));
+	}
+	
+	@Test
+	public void ifDivisibleBy4AndNotBy100ThenIsLeapYear() {
+		assertTrue(leapYears.isLeapYear(2008));
+		assertTrue(leapYears.isLeapYear(2012));
+		assertTrue(leapYears.isLeapYear(2016));
+	}
+	
+	@Test
+	public void ifDivisibleBy4AndBy100ThenIsNotLeapYear() {
+		assertFalse(leapYears.isLeapYear(1800));
+		assertFalse(leapYears.isLeapYear(1900));
+	}
+	
+	@Test
+	public void allYearsNotDivisibleBy4AreNotLeapYears() {
+		assertFalse(leapYears.isLeapYear(2017));
+		assertFalse(leapYears.isLeapYear(2018));
+		assertFalse(leapYears.isLeapYear(2019));
 	}
 
 }
