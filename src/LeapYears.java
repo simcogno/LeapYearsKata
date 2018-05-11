2265d95f0f2cb2eae2f39c20bd1169cee15ced58
Simone <simcogno@gmail.com>
2018-05-11 12:03:39 +0200

    Methods extracted and removed parameter

--- src/LeapYears.java
+++ src/LeapYears.java
@@ -1,15 +1,35 @@
 
 public class LeapYears {
+	
+	private int year;
 
 	public boolean isLeapYear(int year) {
 		
-		if(year < 1582)
+		this.year = year;
+		
+		if(isBeforeGregorianCalendar())
 			return false;
 		
-		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+		if(isDivisibleBy400() || (isDivisibleBy4() && isNotDivisibleBy100()))
 			return true;
 		else
 			return false;
+	}
+
+	private boolean isNotDivisibleBy100() {
+		return year % 100 != 0;
+	}
+
+	private boolean isDivisibleBy4() {
+		return year % 4 == 0;
+	}
+
+	private boolean isDivisibleBy400() {
+		return year % 400 == 0;
+	}
+
+	private boolean isBeforeGregorianCalendar() {
+		return year < 1582;
 	}
 
 }
